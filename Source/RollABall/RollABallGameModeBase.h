7367c24cf699bca1b150bb7dfd7eff05e66ce19a
Teun <teun.willems+github@live.nl>
2017-12-15 22:23:40 +0100

    Done

---
+++ Source/RollABall/RollABallGameModeBase.h
@@ -0,0 +1,20 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "RollABallGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class ROLLABALL_API ARollABallGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
